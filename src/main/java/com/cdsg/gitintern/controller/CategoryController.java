17a418b14e8aae8377321288ace8aafc7be35c23
Han-Lynn-Aung <hanlynnaung1997@gmail.com>
2024-01-03 16:01:08 +0630

    category done

--- src/main/java/com/cdsg/gitintern/controller/CategoryController.java
+++ src/main/java/com/cdsg/gitintern/controller/CategoryController.java
@@ -1,13 +1,15 @@
 package com.cdsg.gitintern.controller;
 
+import com.cdsg.gitintern.exception.categoryexception.CategoryConflictException;
+import com.cdsg.gitintern.exception.categoryexception.CategoryNotFoundException;
+import com.cdsg.gitintern.exception.categoryexception.InvalidCategoryException;
 import com.cdsg.gitintern.model.Category;
 import com.cdsg.gitintern.service.CategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,29 +24,94 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    private static final Logger logger = LoggerFactory.getLogger(CategoryController.class);
+
     @GetMapping
-    public ResponseEntity<List<Category>> getAllCategories(){
-        return new ResponseEntity<>(categoryService.findAllCategories(), HttpStatus.OK);
+    public ResponseEntity<List<Category>> getAllCategory() {
+        logger.info("Retrieving all categories");
+        List<Category> categories = categoryService.findAllCategories();
+        return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
     @GetMapping("/{identifier}")
-    public ResponseEntity<?> getCategoryIdOrName(@PathVariable String identifier){
-        try{
+    public ResponseEntity<?> getCategoryByIdOrName(@PathVariable String identifier) {
+        try {
             long id = Long.parseLong(identifier);
             Optional<Category> categoryById = categoryService.findCategoryById(id);
-            if (categoryById.isPresent()){
+
+            if (categoryById.isPresent()) {
+                logger.info("Category found by ID: {}", id);
                 return ResponseEntity.ok(categoryById.get());
-            }else {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Category not found by id : " + id);
+            } else {
+                logger.error("Category not found by ID: {}", id);
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Category not found by ID: " + id);
             }
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             Optional<Category> categoryByName = categoryService.findCategoryByName(identifier);
-            if(categoryByName.isPresent()){
+
+            if (categoryByName.isPresent()) {
+                logger.info("Category found by name: {}", identifier);
                 return ResponseEntity.ok(categoryByName.get());
-            }else{
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Category not found by name : " + identifier);
+            } else {
+                logger.error("Category not found by name: {}", identifier);
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Category not found by name: " + identifier);
             }
         }
     }
 
+    @PostMapping("/create")
+    public ResponseEntity<?> createCategory(@RequestBody Category category){
+        try{
+            logger.info("Received request to create category: {}" , category );
+            validateCategoryData(category);
+            Category savedCategory = categoryService.saveCategory(category);
+            logger.info("Created category: {}", savedCategory);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+        }catch (InvalidCategoryException e){
+            logger.error("Failed to create due to invalid category data: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateCategory(@PathVariable Long id,@RequestBody Category updateCategory){
+        try{
+            logger.info("Received request to update category with ID {} : {}",id,updateCategory);
+            validateCategoryData(updateCategory);
+            Category updatedCategory = categoryService.updateCategory(id,updateCategory);
+            logger.info("Updated category: {}",updatedCategory);
+            return ResponseEntity.ok(updatedCategory);
+        }catch (CategoryNotFoundException e) {
+            logger.error("Failed to update category as it was not found: {}", e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteCategoryById(@PathVariable Long id){
+        try{
+            logger.info("Received request to delete category with ID: {}",id );
+           Optional<Category> existingCategory = categoryService.findCategoryById(id);
+            if(existingCategory.isPresent()){
+                categoryService.deleteCategoryById(id);
+            }
+            logger.info("Deleted category with ID {}", id);
+            return ResponseEntity.ok("Deleted Category");
+        }catch (CategoryNotFoundException e){
+            logger.error("Failed to delete category as it was not found :{}",e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    private void validateCategoryData(Category category) {
+        if(category.getName() == null){
+            throw new InvalidCategoryException("Invalid category data : Name can not null");
+        }
+        Optional<Category> existingCategory = categoryService.findCategoryByName(category.getName());
+
+        if (existingCategory.isPresent()) {
+            throw new CategoryConflictException("Category with the same name already exists");
+        }
+    }
 }
---
+++ src/main/java/com/cdsg/gitintern/exception/categoryexception/CategoryConflictException.java
@@ -0,0 +1,8 @@
+package com.cdsg.gitintern.exception.categoryexception;
+
+public class CategoryConflictException extends RuntimeException {
+
+    public CategoryConflictException(String message){
+        super(message);
+    }
+}
