47583d4511226e8d07bb7006abd35452d128bbc7
Han-Lynn-Aung <hanlynnaung1997@gmail.com>
2024-01-03 09:32:33 +0630

    initial commit

---
+++ src/main/java/com/cdsg/gitintern/GitInternApplication.java
@@ -0,0 +1,13 @@
+package com.cdsg.gitintern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitInternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitInternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdsg/gitintern/GitInternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdsg.gitintern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitInternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
