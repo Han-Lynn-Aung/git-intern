850bbe775e2ff9c91e8dec4ca8dd8754104bdb7e
Ensiansang <dalepipi18@gmail.com>
2024-01-03 16:21:20 +0630

    Second Commit with update delete findbyid

--- src/main/java/com/cdsg/gitintern/service/ProductService.java
+++ src/main/java/com/cdsg/gitintern/service/ProductService.java
@@ -5,11 +5,17 @@ import com.cdsg.gitintern.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
-    @Autowired
-    private ProductRepository prodRepository;
+
+
+    private final ProductRepository prodRepository;
+
+    public ProductService(ProductRepository prodRepository) {
+        this.prodRepository = prodRepository;
+    }
 
     public Product createNewProduct(Product product) {
         return prodRepository.save(product);
@@ -18,5 +24,17 @@ public class ProductService {
     public List<Product> getAllProduct() {
         return prodRepository.findAll();
     }
+
+    public Optional<Product> getProductById(Long id){
+        return prodRepository.findById(id);
+    }
+
+    public Product updateProduct(Product product) {
+        return prodRepository.save(product);
+    }
+    public void deleteProduct(Long id) {
+        prodRepository.deleteById(id);
+    }
+
 
 }
