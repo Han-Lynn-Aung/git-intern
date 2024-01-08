9e6a009758f39e7affa4212d99d5d89e138ab7a3
Ensiansang <dalepipi18@gmail.com>
2024-01-08 08:29:25 +0630

    jwt&login
    adding jwt&login for user

---
+++ src/main/java/com/cdsg/gitintern/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.cdsg.gitintern.controller;
+
+import com.cdsg.gitintern.model.AuthRequest;
+import com.cdsg.gitintern.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/log-in")
+    public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+            );
+        } catch (Exception ex) {
+            throw new Exception("invalid username/password");
+        }
+        return jwtUtil.generateToken(authRequest.getUsername());
+    }
+
+}
